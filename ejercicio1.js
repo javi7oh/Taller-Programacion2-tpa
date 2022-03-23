0c0daebff23735fd3c41fc7439dfa6d58e390548
JavierToso <ortjavier1995@gmail.com>
2022-03-22 22:18:06 -0300

    Ejercicio 1 - Javier Toso
    Ejercicio 1 - Console.log typeof lista

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,1 +1,13 @@
-console.log("Hola Mundo :D");
+let lista = [
+    "hola", 
+    123,
+    false,
+    undefined,
+    null,
+    "final",
+];
+
+
+for (let index = 0; index < lista.length; index++) {
+    console.log(typeof lista[index]);
+}
